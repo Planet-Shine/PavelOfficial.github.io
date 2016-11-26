94b62acd3ae61899bcb72e91a83fbd4d9676f2a6
PavelOfficial <pavelofficial@ya.ru>
2016-11-27 00:58:00 +0300

    portfolio workpiece

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,67 +1,70 @@
 var gulp = require('gulp'),
     bundle = require('gulp-bundle-assets'),
     ejs = require("gulp-ejs"),
-    html2js = require('gulp-html2js'),
-    server = require('gulp-develop-server'),
-    open = require('gulp-open');
+    concat = require("gulp-concat");
 
-
-gulp.task('bundle', ['templates'], function () {
+gulp.task('bundle', function () {
     return gulp.src('./bundle.config.js')
         .pipe(bundle())
         .pipe(bundle.results({
             pathPrefix: './static/'
         }))
-        .pipe(gulp.dest('./app/static'));
+        .pipe(gulp.dest('../static/'));
 });
 
 gulp.task('pageCompile', ['bundle'], function () {
-    var bundleResult =  require('./bundle.result.json');
-    return gulp.src("./app/templates/*.ejs")
-        .pipe(ejs({
-            scripts : [bundleResult['vendor']['scripts'], bundleResult['main']['scripts']].join(''),
-            styles  : bundleResult['main']['styles']
-        },{
-            ext : '.html'
-        }))
-        .pipe(gulp.dest("./app"));
+    var bundleResult =  require('./bundle.result.json'),
+        targetFolder = '../samples/',
+        fs = require('fs'),
+        pageItemLimit = 1,
+        files,
+        descriptions,
+        pages = [];
+
+    files = fs.readdirSync(targetFolder);
+    files = files.map(function (file) {
+        return targetFolder + file + '/description.json';
+    });
+    files = files.filter(function (file) {
+        return fs.existsSync(file);
+    });
+    descriptions = files.map(function (file) {
+        return require(file);
+    });
+
+    while (descriptions.length) {
+        pages.push(descriptions.splice(0, pageItemLimit));
+    }
+    pages.forEach(function (experienceList, index) {
+        gulp.src("./app/templates/experienceList.ejs")
+            .pipe(ejs({
+                scripts        : [bundleResult['vendor']['scripts'], bundleResult['main']['scripts']].join(''),
+                styles         : bundleResult['main']['styles'],
+                experienceList : experienceList,
+                pageCount      : pages.length
+            }, {
+                ext : '.html'
+            }))
+            .pipe(concat("page-" + (index + 1) + ".html"))
+            .pipe(gulp.dest("../experiences/"));
+    });
+
+    // todo : template compilation
+    /*
+        return gulp.src("./app/templates/*.ejs")
+            .pipe(ejs({
+                scripts : [bundleResult['vendor']['scripts'], bundleResult['main']['scripts']].join(''),
+                styles  : bundleResult['main']['styles']
+            },{
+                ext : '.html'
+            }))
+            .pipe(gulp.dest("./app"));
+    */
 });
 
 gulp.task('copyStatic', function () {
     return gulp.src("./node_modules/bootstrap/fonts/**/*.*", {base : './node_modules/bootstrap/'})
-        .pipe(gulp.dest("./app/"));
+        .pipe(gulp.dest("../static/"));
 });
 
-gulp.task('templates', function () {
-    return gulp.src('./app/js/**/*.html')
-        .pipe(html2js('employeeTemplates.js', {
-            adapter : 'angular',
-            base : './app',
-            name : 'employeeTemplates'
-        }))
-        .pipe(gulp.dest('./app/templates'));
-});
-
-gulp.task('runServer', ['buildProject'], function (callback) {
-    server.listen({
-        path : './node_modules/http-server/bin/http-server',
-        args :  ['-a', 'localhost', '-p', '8000', '-c-1', './app']
-    });
-    setTimeout(function () {
-        callback(null);
-    }, 500);
-});
-
-gulp.task('buildProject', ['pageCompile'], function (callback) {
-    callback(null);
-});
-
-gulp.task('runApplication', ['runServer'], function () {
-    gulp.src(__filename)
-        .pipe(open({
-            app : 'chrome',
-            uri : 'http://localhost:8000/'
-        }));
-});
-
-gulp.task('default', ['templates', 'pageCompile', 'runApplication', 'runServer', 'buildProject', 'runApplication']);
+gulp.task('default', ['pageCompile', 'bundle']);
